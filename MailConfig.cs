0ea053b8941abe05a08f05fd5f07420bd1af0b17
Grace L <Grace@hypnovir.us>
2023-05-03 00:18:25 -0700

    Initial commit that matters.

---
+++ MailConfig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FediMail
+{
+    internal class MailConfig
+    {
+        public string EmailAddress { get; set; }
+        public string EmailPassword { get; set; }
+        public string ImapHost { get; set; }
+        public string SmtpHost { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,84 @@
+﻿using CliWrap;
+using CliWrap.Buffered;
+using MailKit;
+using MailKit.Net.Imap;
+using MimeKit;
+using System.Configuration;
+
+namespace FediMail
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var mailConfig = new MailConfig
+            {
+                EmailAddress = ConfigurationManager.AppSettings["emailAddress"],
+                EmailPassword = ConfigurationManager.AppSettings["emailPassword"],
+                ImapHost = ConfigurationManager.AppSettings["imapHost"],
+                SmtpHost = ConfigurationManager.AppSettings["smtpHost"]
+            };
+
+            var msyncPath = ConfigurationManager.AppSettings["mysncPath"];
+
+            using (var client = new ImapClient())
+            {
+                await client.ConnectAsync(mailConfig.ImapHost, 0, true);
+                await client.AuthenticateAsync(mailConfig.EmailAddress, mailConfig.EmailPassword);
+
+                var inbox = client.Inbox;
+                await inbox.OpenAsync(MailKit.FolderAccess.ReadWrite);
+
+                Console.WriteLine("Total messages: {0}", inbox.Count);
+                Console.WriteLine("Recent messages: {0}", inbox.Recent);
+
+                try
+                {
+                    for (int i = 0; i < inbox.Count; i++)
+                    {
+                        var message = inbox.GetMessage(i);
+                        var tempPath = WritePostFile(message);
+
+                        // check this result
+                        // reply with output
+                        var result = await Cli.Wrap(msyncPath)
+                            .WithArguments(new[] { "queue", tempPath })
+                            .ExecuteBufferedAsync();
+
+                        Console.WriteLine(result.StandardOutput);
+
+                        await inbox.StoreAsync(i, new StoreFlagsRequest(StoreAction.Add, MessageFlags.Deleted) { Silent = true });
+                    }
+                }
+                finally
+                {
+                    inbox.Expunge();
+                    client.Disconnect(true);
+                }
+            }
+
+        }
+
+        private static string WritePostFile(MimeMessage message)
+        {
+            var tempFile = Path.GetTempFileName();
+
+            var toWrite = new List<string>
+            {
+                "visibility=default",
+                "--- post body below this line ---",
+                message.GetTextBody(MimeKit.Text.TextFormat.Plain)
+            };
+
+            if (!string.IsNullOrWhiteSpace(message.Subject))
+                toWrite.Insert(0, $"cw={message.Subject}");
+
+            File.WriteAllLines(tempFile, toWrite, System.Text.Encoding.UTF8);
+
+            return tempFile;
+        }
+
+
+
+    }
+}
